a59ecfdfbac63808e13641dbbd2099722c8c52cd
MarcisKr <marcis.vitols@testdevlab.com>
2019-09-23 14:54:43 +0300

    made so report shows what browser was used for the run

--- report_builder.rb
+++ report_builder.rb
@@ -2,6 +2,14 @@ require 'report_builder'
 
 time = Time.now.getutc
 
+if ARGV[1] == "chrome=true"
+    browser = "Chrome"
+elsif ARGV[1] == "firefox=true"
+    browser = "Firefox"
+elsif ARGV[1] == "safari=true"
+    browser = "Safari"
+end
+
 ReportBuilder.configure do |config|
  config.json_path = 'report.json'
  config.report_path = 'cucumber_web_report'
@@ -9,7 +17,7 @@ ReportBuilder.configure do |config|
  config.report_tabs = %w[Overview Features Scenarios Errors]
  config.report_title = 'Cucumber web automation test results'
  config.compress_images = false
- config.additional_info = { 'Project name' => 'UniCredit Leasing', 'Platform' => 'Test', 'Report generated' => time }
+ config.additional_info = { 'Project name' => 'UniCredit Leasing', 'Browser' => browser, 'Platform' => 'Test', 'Report generated' => time }
 end
 
 ReportBuilder.build_report
